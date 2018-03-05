973435a589ac1cb04a3a6a08aceb1a44d209be91
Michael Brown <producer@holotronic.dk>
2018-03-05 15:26:20 +0100

    updated to v 17.1 and purge redundant files

--- cv_megawizard/audio_pll_sim/aldec/rivierapro_setup.tcl
+++ cv_megawizard/audio_pll_sim/aldec/rivierapro_setup.tcl
@@ -1,5 +1,5 @@
 
-# (C) 2001-2017 Altera Corporation. All rights reserved.
+# (C) 2001-2018 Altera Corporation. All rights reserved.
 # Your use of Altera Corporation's design tools, logic functions and 
 # other software and tools, and its AMPP partner logic functions, and 
 # any output files any of the foregoing (including device programming 
@@ -12,7 +12,7 @@
 # or its authorized distributors. Please refer to the applicable 
 # agreement for further details.
 
-# ACDS 17.1 590 linux 2017.12.25.15:50:42
+# ACDS 17.1 593 linux 2018.02.20.13:26:18
 # ----------------------------------------
 # Auto-generated simulation script rivierapro_setup.tcl
 # ----------------------------------------
--- cv_megawizard/audio_pll_sim/mentor/msim_setup.tcl
+++ cv_megawizard/audio_pll_sim/mentor/msim_setup.tcl
@@ -1,5 +1,5 @@
 
-# (C) 2001-2017 Altera Corporation. All rights reserved.
+# (C) 2001-2018 Altera Corporation. All rights reserved.
 # Your use of Altera Corporation's design tools, logic functions and 
 # other software and tools, and its AMPP partner logic functions, and 
 # any output files any of the foregoing (including device programming 
@@ -94,7 +94,7 @@
 # within the Quartus project, and generate a unified
 # script which supports all the Altera IP within the design.
 # ----------------------------------------
-# ACDS 17.1 590 linux 2017.12.25.15:50:42
+# ACDS 17.1 593 linux 2018.02.20.13:26:18
 
 # ----------------------------------------
 # Initialize variables
