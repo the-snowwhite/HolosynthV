c152e2d4b9f2857e8a71cbb45f3dde514bf6cfbf
Michael Brown <producer@holotronic.dk>
2020-04-17 23:03:56 +0200

    Add updated hardware file and fix compile problem
    Signed-off-by: Michael Brown <producer@holotronic.dk>

--- VivadoProjects/Ultra96_Holosynth/Ultra96_Holosynth.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.v
+++ VivadoProjects/Ultra96_Holosynth/Ultra96_Holosynth.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2019.2 (lin64) Build 2708876 Wed Nov  6 21:39:14 MST 2019
-//Date        : Sat Apr  4 15:11:30 2020
+//Date        : Fri Apr 17 20:13:54 2020
 //Host        : kdeneon-ws running 64-bit Ubuntu 18.04.4 LTS
 //Command     : generate_target design_1_wrapper.bd
 //Design      : design_1_wrapper
